7e94e14235717442a3e15a20e00e04ed7c3ddce5
Marc Alfonso <marc.alfonso@balmes.epiaedu.cat>
2018-05-17 10:21:37 +0200

    Working on script

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-adsfdasdfasº
+const discord = require("discord.js");
+const cliente = new Discord.Client();
+const ascii_art = require("ascii-art");
+
+var token;
+
+cliente.on('ready', () => {
+  //console.log(`Logged in as ${client.user.tag}!`);
+});
+
+cliente.on('message', msg => {
+  if (msg.content.indexOf(ascii_art) == 0) {
+	  console.log(msg.content.slice(7, msg.content.length));
+	  var palabra = ascii_art.font(msg.content.slice
+								  (7, msg.content.length),
+								  'Graffiti')
+  }
+});
