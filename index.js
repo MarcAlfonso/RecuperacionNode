1f960c6ede24da980cbcade95e7c94376583e859
Marc Alfonso <marc.alfonso@balmes.epiaedu.cat>
2018-05-17 11:31:02 +0200

    Having issues with the command...

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const discord = require("discord.js");
-const cliente = new Discord.Client();
-const ascii_art = require("ascii-art");
+const discord = require('discord.js');
+const cliente = new discord.Client();
+const ascii = require('ascii-art');
 
-var token;
+var token = "NDQ2NTg5Nzk0NTg0MTAwODcw.Dd7c1w.IOr7Z6IFec1LiFpStjNuv_BCa24";
 
 cliente.on('ready', () => {
-  //console.log(`Logged in as ${client.user.tag}!`);
+  console.log('Logged into the server!');
 });
 
 cliente.on('message', msg => {
-  if (msg.content.indexOf(ascii_art) == 0) {
-	  console.log(msg.content.slice(7, msg.content.length));
-	  var palabra = ascii_art.font(msg.content.slice
-								  (7, msg.content.length),
-								  'Graffiti')
+  if (msg.content == "!ascii") {
+	  console.log("Received ascii command: " + msg.content.slice(7, msg.content.length));
+	  var palabra = ascii.font(msg.content.slice (7, msg.content.length),'Graffiti', function(rendered){
+		console.log(rendered);
+		msg.reply('\n' + rendered);
+	  });
+	  console.log(palabra);
   }
 });
+cliente.login(token);
