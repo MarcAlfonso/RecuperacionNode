16356ea44e7fe5f2a2274c77d5c402beae1b4df6
Marc Alfonso <marc.alfonso@balmes.epiaedu.cat>
2018-05-17 12:40:21 +0200

    Seems that now works!

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const discord = require('discord.js');
 const cliente = new discord.Client();
-const ascii = require('ascii-art');
+const art = require('ascii-art');
 
 var token = "NDQ2NTg5Nzk0NTg0MTAwODcw.Dd7c1w.IOr7Z6IFec1LiFpStjNuv_BCa24";
 
 cliente.on('ready', () => {
-  console.log('Logged into the server!');
+  console.log('Te has logeado en el servidor!');
 });
 
 cliente.on('message', msg => {
-  if (msg.content == "!ascii") {
-	  console.log("Received ascii command: " + msg.content.slice(7, msg.content.length));
-	  var palabra = ascii.font(msg.content.slice (7, msg.content.length),'Graffiti', function(rendered){
-		console.log(rendered);
-		msg.reply('\n' + rendered);
+  if (msg.content.indexOf('!art') == 0) {
+	  console.log(msg.content.slice(5, msg.content.length));
+	  var palabra = art.font(msg.content.slice(5, msg.content.length),'Rusted', render =>{
+		console.log(render);
+		msg.reply('\n' + render);
 	  });
 	  console.log(palabra);
   }
